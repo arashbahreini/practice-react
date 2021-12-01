e5b48b6c7ad6fd34fad770759db25309f7814aad
unknown <vbhost.ir@gmail.com>
2021-12-01 11:47:15 +0100

    fixed abs import issue

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
-import {Routes, Route, Link} from "react-router-dom";
-import {Navigation} from './components/core/Navigation/Navigation';
-import {routes} from "./config/routes";
+import { Routes, Route, Link } from "react-router-dom";
+import { Navigation } from './components/core/Navigation/Navigation';
+import { routes } from "./config/routes";
 
 function App() {
     return (
         <div className="App">
-            <Navigation/>
+            <Navigation />
             <Routes>
                 {routes.map((route) => {
                     console.log('route', route);
-                    return <Route path={route.path} element={route.component} key={route.name}/>
+                    return <Route path={route.path} element={route.component} key={route.name} />
                 })}
             </Routes>
         </div>
