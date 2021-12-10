e604fff34c066bdba2a86ce39b07d487db13e56f
unknown <vbhost.ir@gmail.com>
2021-12-10 10:28:47 +0100

    changed navigation html elements to nav

--- src/components/core/Navigation/Navigation.js
+++ src/components/core/Navigation/Navigation.js
@@ -4,18 +4,20 @@ import styles from './Navigation.module.css'
 
 export const Navigation = () => (
     <nav>
-        <div className={styles.navContainer}>
-            {routes.map(item => {
-                return (
-                    <span key={item.name}>
-                        {/* What is key? */}
-                        <Link to={item.path} underline="none">
-                            {/*Todo: add icon here*/}
-                            {item.name}
-                        </Link>
-                    </span>
-                );
-            })}
-        </div>
+        <nav className={styles.navContainer}>
+            <ul>
+                {routes.map(item => {
+                    return (
+                        <li key={item.name}>
+                            {/* What is key? */}
+                            <Link to={item.path} underline="none">
+                                {/*Todo: add icon here*/}
+                                {item.name}
+                            </Link>
+                        </li>
+                    );
+                })}
+            </ul>
+        </nav>
     </nav>
 );
