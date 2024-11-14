5b4378dd7a598fbc0a4a78efde01f9f1a2890a9a
Arash <arash.bahreini@beerwulf.com>
2024-11-14 10:32:48 +0100

    initial commit with js project

---
+++ src/pages/layout.js
@@ -0,0 +1,10 @@
+
+export default function Layout({ children }) {
+    return (
+        <>
+            <h1>Header</h1>
+            <main>{children}</main>
+            <h1>Footer</h1>
+        </>
+    )
+}
